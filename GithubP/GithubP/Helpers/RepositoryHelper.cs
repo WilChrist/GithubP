3caaa4064dd6419512f983b15c509aa1585b9390
Willy Nzesseu <willynzesseu@gmail.com>
2018-11-20 08:26:50 +0100

    feat(retrieving data): add models
    creation of model for encapsulate data retrieved from API

---
+++ GithubP/GithubP/Helpers/RepositoryHelper.cs
@@ -0,0 +1,16 @@
+﻿using GithubP.Models.Repository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubP.Helpers
+{
+    public static class RepositoryHelper
+    {
+        
+    }
+}
